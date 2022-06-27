d85095dfd5097c0f6a0db9a25344087c93732baa
fnsunisa7 <fnsunisa7@gmail.com>
2022-06-27 20:33:04 +0000

    add css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import '../app.config';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,93 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { Container, Box, Badge } from '@chakra-ui/react'
+import { BiStar } from "react-icons/bi";
 
 const Home: NextPage = () => {
+  const property = {
+    imageUrl: 'https://bit.ly/2Z4KKcF',
+    imageAlt: 'Rear view of modern home with pool',
+    beds: 3,
+    baths: 2,
+    title: 'Modern home in city center in the heart of historic Los Angeles',
+    formattedPrice: '$1,900.00',
+    reviewCount: 34,
+    rating: 4,
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Blog -&gt; fnsunisa7</title>
         <meta name="description" content="my blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Container maxW='2xl' centerContent>
+        <Box padding='4' maxW='md'>
+          There are many benefits to a joint design and development system. Not only
+          does it bring benefits to the design team, but it also brings benefits to
+          engineering teams. It makes sure that our experiences have a consistent look
+          and feel, not just in our design specs, but in production.
+        </Box>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+          <img src={property.imageUrl} alt={property.imageAlt} />
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <Box p='6'>
+            <Box display='flex' alignItems='baseline'>
+              <Badge borderRadius='full' px='2' colorScheme='teal'>
+                New
+              </Badge>
+              <Box
+                color='gray.500'
+                fontWeight='semibold'
+                letterSpacing='wide'
+                fontSize='xs'
+                textTransform='uppercase'
+                ml='2'
+              >
+                {property.beds} beds &bull; {property.baths} baths
+              </Box>
+            </Box>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+            <Box
+              mt='1'
+              fontWeight='semibold'
+              as='h4'
+              lineHeight='tight'
+              noOfLines={1}
+            >
+              {property.title}
+            </Box>
+
+            <Box>
+              {property.formattedPrice}
+              <Box as='span' color='gray.600' fontSize='sm'>
+                / wk
+              </Box>
+            </Box>
+
+            <Box display='flex' mt='2' alignItems='center'>
+              {Array(5)
+                .fill('')
+                .map((_, i) => (
+                  <BiStar
+                    key={i}
+                    color={i < property.rating ? 'teal.500' : 'gray.300'}
+                  />
+                ))}
+              <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                {property.reviewCount} reviews
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+
     </div>
   );
 }
